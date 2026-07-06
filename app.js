91e7e820dcedbce1c942a007e4f5cf3f23da14ec
arataarataarata <arataarataarata@gmail.com>
2026-07-06 18:00:11 +0900

    Add showScreen function to manage screen visibility

---
+++ app.js
@@ -0,0 +1,7 @@
+function showScreen(name) {
+  document.querySelectorAll(".screen").forEach(screen => {
+    screen.classList.remove("active");
+  });
+
+  document.getElementById(`screen-${name}`).classList.add("active");
+}
